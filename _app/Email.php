317311523833b2aea645829b9563f4e04ab7f8d4
Richard de Deus <richard.deus@mi-agenciaweb.com>
2017-11-13 14:01:34 -0200

    library ok

--- _app/Email.php
+++ _app/Email.php
@@ -11,21 +11,21 @@ class Email{
 
     
     
-    public function __construct() {
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName) {
         $this->mail = new PHPMailer(true);  
         //Server settings
-        $this->mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $this->mail->SMTPDebug = $smtpDebug;                        // Enable verbose debug output
         $this->mail->isSMTP();                                      // Set mailer to use SMTP
-        $this->mail->Host = 'mail.mi-agenciaweb.com';  // Specify main and backup SMTP servers
+        $this->mail->Host = $host;                                  // Specify main and backup SMTP servers
         $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $this->mail->Username = 'contato@mi-agenciaweb.com';                 // SMTP username
-        $this->mail->Password = 'rd@at1902';                           // SMTP password
-        $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $this->mail->Port = 465;                                    // TCP port to connect to
+        $this->mail->Username = $user;                              // SMTP username
+        $this->mail->Password = $pass;                              // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                      // Enable TLS encryption, `ssl` also accepted
+        $this->mail->Port = $port;                                  // TCP port to connect to
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->setFrom('richard.deus@mi-agenciaweb.com', 'Richard de Deus');
+        $this->mail->setFrom($setFromEmail, $setFromName);
 
     }
 
--- teste/index.php
+++ teste/index.php
@@ -1,11 +1,11 @@
 <?php
-require __DIR__.'/lib_ext/autoload.php';
+require __DIR__.'/../lib_ext/autoload.php';
 
 //instancia namespace e classe
 use Notification\Email;
 
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, 'mail.mi-agenciaweb.com', 'contato@mi-agenciaweb.com', 'rd@at1902', 'tls', '587', 'richard.deus@tarjaconsult.com', "Richard Tarjaconsult");
 $novoEmail->sendMail("teste composer", "<p>Esse´é um e-mail de teste do curso de <b>composer</b></p>","richard.deus@tarjaconsult.com", "Richard de deus", "richard.deus@gmail.com", "Richard Gmail" );
 
 var_dump($novoEmail);
